8dca1b5a4caeebd3bf1108735f8c6cdef3d74ff3
Anthony Ossai <ossaiant01@gmail.com>
2026-02-02 02:51:48 +0000

    Add new Playwright test files and update configuration
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch main
    # Changes to be committed:
    #	modified:   playwright.config.js
    #	new file:   tests/ClientApp.spec.js
    #	deleted:    tests/PageBrowserContext.spec.js
    #	modified:   tests/UIBasicstest.spec.js
    #	new file:   tests/UIControls.spec.js
    #	new file:   tests/textContentValueInput.spec.js
    #

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,24 +1,24 @@
 const {test, expect} = require('@playwright/test');
 
 //test.use({ browserName: 'webkit'});
- test('@Web Browser Context-Validating Error login', async ({browser})=>
+ test.only('@Web Browser Context-Validating Error login', async ({browser})=>
  {
 
-   const context = await browser.newContext();
-      const page =  await context.newPage();
+    const context = await browser.newContext();
+    const page =  await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    console.log(await page.title());
     // page.route('**/*.{jpg,png,jpeg}',route=> route.abort());
-      const userName = page.locator('#username');
-      const signIn = page.locator("#signInBtn");
-      const cardTitles =  page.locator(".card-body a");
+    const userName = page.locator('#username');
+    const signIn = page.locator("#signInBtn");
+    // const cardTitles =  page.locator(".card-body a");
     //   page.on('request',request=> console.log(request.url()));
     //   page.on('response',response=> console.log(response.url(), response.status()));
-      await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     //   console.log(await page.title());
-
-     await userName.fill("rahulshetty");
-     await page.locator("[type='password']").fill("learning");
+     await userName.fill("rahulshettyacademy");
+     await page.locator("[type='password']").fill("Learning@830$3mK2");
      await signIn.click();
     console.log(await page.locator("[style*='block']").textContent());
     await expect(page.locator("[style*='block']")).toContainText('Incorrect');
-    
+
  });
---
+++ tests/UIControls.spec.js
@@ -0,0 +1,45 @@
+ const {test, expect} = require('@playwright/test');
+
+ test('@Web UI Controls', async ({page})=>
+ {
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const userName = page.locator('#username');
+    const signIn = page.locator("#signInBtn");
+    const documentLink = page.locator("[href*='documents-request']");
+    const dropdown = page.locator("select.form-control");
+    await dropdown.selectOption("consult");
+    await page.locator(".radiotextsty").last().click();
+    await page.locator("#okayBtn").click();
+    console.log(await page.locator(".radiotextsty").last().isChecked());
+    await expect(page.locator(".radiotextsty").last()).toBeChecked();
+    await page.locator("#terms").click();
+    await expect( page.locator("#terms")).toBeChecked();
+    await page.locator("#terms").uncheck();
+    expect( await page.locator("#terms").isChecked()).toBeFalsy();
+    await expect(documentLink).toHaveAttribute("class","blinkingText");
+ });
+
+
+  test.only('@Child windows hadl', async ({browser})=>
+ {
+    const context = await browser.newContext();
+    const page =  await context.newPage();
+    const userName = page.locator('#username');
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const documentLink = page.locator("[href*='documents-request']");
+
+    const [newPage]=Promise.all(
+   [
+      context.waitForEvent('page'),//listen for any new page pending,rejected,fulfilled
+      documentLink.click(),
+   
+   ])//new page is opened
+    
+    text = await newPage.locator(".red").textContent();
+    const arrayText = text.split("@")
+    const domain =  arrayText[1].split(" ")[0]
+    console.log(domain);
+    await page.locator("#username").fill(domain);
+    console.log(await page.locator("#username").textContent());
+
+    });
